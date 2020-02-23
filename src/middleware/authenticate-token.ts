8af4008db5ae48ff017c85482ed2cb94db720ad9
Ukiyo Roa <jnpco.98@gmail.com>
2020-02-23 21:51:25 +1100

    Added Bearer Auth parser middleware

---
+++ src/middleware/authenticate-token.ts
@@ -0,0 +1,27 @@
+import { verify } from 'jsonwebtoken';
+import { Request, Response, NextFunction } from 'express';
+
+import { User } from '../entity/user';
+import ROLES from '../constants/roles';
+
+export async function authenticateToken(req: Request, res: Response, next: NextFunction) {
+  const authHeader = req.headers.authorization;
+  const accessToken = authHeader && authHeader.split(' ')[1];
+
+  req.auth = { ...req.auth, role: ROLES.anonymous, userId: null };
+
+  if(accessToken) {
+    try {
+      const decoded = verify(accessToken, process.env.ACCESS_TOKEN_SECRET!) as { userId: number, role: string; };
+      const user = await User.findOne(decoded.userId);
+
+      if(user && user.role === decoded.role) {
+        req.auth = { ...req.auth, role: decoded.role, userId: decoded.userId };
+      }
+    } catch(e) {
+      req.auth = { ...req.auth, role: ROLES.anonymous };
+    }
+  }
+
+  next();
+}
