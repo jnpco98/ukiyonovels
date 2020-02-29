e5c09b5109bdcd4e366b1fef2a7ad478bad74d21
Ukiyo Roa <jnpco.98@gmail.com>
2020-03-01 10:41:27 +1100

    Implemented token limits per user.

--- src/resolvers/authentication/create-token.ts
+++ src/resolvers/authentication/create-token.ts
@@ -4,22 +4,33 @@ import { User } from "../../entity/user";
 import { generateTokens } from '../../utilities/auth/token';
 import { AuthTokens } from '../../entity/token';
 import { TokenCreateInput } from './token-base';
+import { REFRESH_TOKEN_LIMIT } from '../../constants/token';
 
 @Resolver()
 export class TokenCreateResolver {  
-  @Mutation(returns => AuthTokens, { name: 'tokenCreate',nullable: true })
+  @Mutation(returns => AuthTokens, { name: 'tokenCreate', nullable: true })
   async createToken(
     @Arg('data') { password, email }: TokenCreateInput
   ): Promise<AuthTokens | null> {
     const user = await User.findOne({ where: { email } });
     const userIsValid = await bcrypt.compare(password, user?.password || '');
     if(!user || !user.confirmed || !userIsValid) return null;
+
+    const userActiveTokensCount = await AuthTokens.count({ 
+      where: { 
+        creatorId: user.id, 
+        archived: false 
+      } 
+    });
+
+    if(userActiveTokensCount + 1 > REFRESH_TOKEN_LIMIT) return null;
     
     const { accessToken, refreshToken } = generateTokens(user);
 
     const tokens = new AuthTokens();
     tokens.refreshToken = refreshToken;
     tokens.accessToken = accessToken;
+    tokens.creatorId = user.id;
     await tokens.save();
     
     return tokens;
