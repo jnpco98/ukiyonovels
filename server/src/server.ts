47b4b475a94a46eaffde6b5904c24f8a17a52ced
Ukiyo Roa <jnpco.98@gmail.com>
2020-03-14 21:37:11 +1100

    Moved graphqlformaterror to a new function

--- server/src/server.ts
+++ server/src/server.ts
@@ -18,23 +18,31 @@ import { isDevelopment, isTesting, isProduction, ENV_DEVELOPMENT, ENV_TESTING, E
 import { ArgumentValidationError } from 'type-graphql';
 import { logInternalError } from './utilities/error/log-internal';
 
+function formatGraphqlError (error: GraphQLError) {
+  if(error.originalError instanceof ApolloError) return error;
+  if(error.originalError instanceof ArgumentValidationError) {
+    if(error.extensions) error.extensions.code = 'GRAPHQL_VALIDATION_FAILED';
+    return error;
+  }
+
+  return new GraphQLError(`Internal Server Error: ${logInternalError(error)}`);
+}
+
 async function main() {
   const connection = await initializeConnection();
 
   /**
    * NODE_ENV must be setup
    */
-  if(!isDevelopment() && !isTesting() && !isProduction()) {
+  if(!isDevelopment() && !isTesting() && !isProduction()) 
     throw new Error(`NODE_ENV must be ${ENV_DEVELOPMENT} || ${ENV_TESTING} || ${ENV_PRODUCTION}`);
-  }
   
   /**
    * Only run database migrations on production
    * as this might destroy existing data
    */
-  if(!isProduction) {
+  if(!isProduction) 
     await connection.runMigrations();
-  }
 
   const MAX_QUERY_COST = parseInt(process.env.MAX_QUERY_COST || '1000');
 
@@ -42,15 +50,7 @@ async function main() {
   const server = new ApolloServer({
     schema,
     context: ({ req, res }) => ({ req, res }),
-    formatError: (error) => {
-      if(error.originalError instanceof ApolloError) return error;
-      if(error.originalError instanceof ArgumentValidationError) {
-        if(error.extensions) error.extensions.code = 'GRAPHQL_VALIDATION_FAILED';
-        return error;
-      }
-
-      return new GraphQLError(`Internal Server Error: ${logInternalError(error)}`);
-    },
+    formatError: formatGraphqlError,
     plugins: [
       {
         requestDidStart: () => ({
