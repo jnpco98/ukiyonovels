a2f1c72e9ac17bdab781f1fb567795777b606a55
ukiyo-roa <jnpco.98@gmail.com>
2020-05-12 12:29:43 +1000

    Used env files for production config typeorm

--- server/src/utilities/connection/initialize-connection.ts
+++ server/src/utilities/connection/initialize-connection.ts
@@ -1,16 +1,42 @@
 import { getConnectionOptions, createConnection } from 'typeorm';
 import { DropDatabaseGuardOptions } from './types/drop-database-guard-options';
+import { PostgresConnectionOptions } from 'typeorm/driver/postgres/PostgresConnectionOptions';
+import { isProduction } from '../env/node-env';
 
 export async function initializeConnection(
   dropDatabaseGuardOptions: DropDatabaseGuardOptions = {}
 ) {
+  const username = process.env.POSTGRES_USER!; 
+  const password = process.env.POSTGRES_PASSWORD!;
+  const host = process.env.POSTGRES_HOST!;
+  const port = process.env.POSTGRES_PORT!;
+  const database = process.env.POSTGRES_DATABASE!
+
+  if(isProduction() && !username || !password || !host || !port || !database) 
+    throw new Error(`Database config not setup`);
+
   /**
    * Gets the database configuration
    * from the environment
    */
   const { drop, databaseToDrop } = dropDatabaseGuardOptions;
   const configuration = await getConnectionOptions(process.env.NODE_ENV);
-  const connectionParams = { ...configuration, name: 'default' };
+
+  const connectionParams: { [key: string]: any } = { 
+    ...configuration, 
+    name: 'default'
+  };
+
+  /**
+   * Use env config on production
+   */
+  if(isProduction()) {
+    connectionParams.username = username;
+    connectionParams.password = password;
+    connectionParams.host = host;
+    connectionParams.port = port;
+    connectionParams.database = database;
+  }
 
   /**
    * If database is set to drop
@@ -21,6 +47,6 @@ export async function initializeConnection(
     connectionParams.dropSchema = drop;
   }
 
-  const connection = await createConnection(connectionParams);
+  const connection = await createConnection(connectionParams as PostgresConnectionOptions);
   return connection;
 }
